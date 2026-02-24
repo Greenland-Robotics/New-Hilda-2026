f2c9486da28059120ccb5419be8582ecb8f261dc
Josh Kelley <josh9kelley@gmail.com>
2026-02-24 10:48:03 -0500

    initialize from quickstart

--- TeamCode/src/main/java/gcsrobotics/pedroPathing/Tuning.java
+++ TeamCode/src/main/java/gcsrobotics/pedroPathing/Tuning.java
@@ -1,11 +1,11 @@
-package org.firstinspires.ftc.teamcode.pedroPathing;
+package gcsrobotics.pedroPathing;
 
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.changes;
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.drawOnlyCurrent;
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.draw;
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.follower;
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.stopRobot;
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.telemetryM;
+import static gcsrobotics.pedroPathing.Tuning.changes;
+import static gcsrobotics.pedroPathing.Tuning.drawOnlyCurrent;
+import static gcsrobotics.pedroPathing.Tuning.draw;
+import static gcsrobotics.pedroPathing.Tuning.follower;
+import static gcsrobotics.pedroPathing.Tuning.stopRobot;
+import static gcsrobotics.pedroPathing.Tuning.telemetryM;
 
 import com.bylazar.configurables.PanelsConfigurables;
 import com.bylazar.configurables.annotations.Configurable;
@@ -15,7 +15,6 @@ import com.bylazar.field.PanelsField;
 import com.bylazar.field.Style;
 import com.bylazar.telemetry.PanelsTelemetry;
 import com.bylazar.telemetry.TelemetryManager;
-import com.pedropathing.ErrorCalculator;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.*;
 import com.pedropathing.math.*;
